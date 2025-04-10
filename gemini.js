a2baf0bcfdfedd65c1f81c95d3e445604d317fb0
fabiod89 <40812098+fabiod89@users.noreply.github.com>
2025-04-10 10:38:28 -0400

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -7,7 +7,7 @@ const {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.0-flash-lite",
   });
   
   const generationConfig = {
